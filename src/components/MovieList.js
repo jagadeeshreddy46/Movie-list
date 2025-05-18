58e9cd4ee918ebd0e37f69d23b62ed7c00bb8c09
Jagadesh-creator <mekalajagadeeswarreddy@gmail.com>
2025-05-18 23:17:37 +0530

    first

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,23 +1,23 @@
 import React, { useEffect, useState } from "react";
 import "./movieList.css";
 import { useParams } from "react-router-dom";
-import Card from "../components/Cards"; // Updated import for correct case
+import Card from "../components/Cards";
 
 const MovieList = () => {
   const [movieList, setMovieList] = useState([]);
   const { type } = useParams();
 
   useEffect(() => {
-    getData();
-  }, [type]); // Combined useEffect calls
+    const getData = () => {
+      fetch(
+        `https://api.themoviedb.org/3/movie/${type || "popular"}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`
+      )
+        .then((res) => res.json())
+        .then((data) => setMovieList(data.results));
+    };
 
-  const getData = () => {
-    fetch(
-      `https://api.themoviedb.org/3/movie/${type || "popular"}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`
-    )
-      .then((res) => res.json())
-      .then((data) => setMovieList(data.results));
-  };
+    getData();
+  }, [type]); // Only re-run when 'type' changes
 
   return (
     <div className="movie__list">
